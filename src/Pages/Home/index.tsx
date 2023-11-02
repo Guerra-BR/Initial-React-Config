f21014bae61e25c57cfa0088782e651d213b244d
Guerra-BR <guerinohenrique@gmail.com>
2023-11-02 11:43:44 -0300

    type example

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import logo from '../../logo.svg'
 import ParticlesBg from '../../components/Particles'
+import { useTypewriter } from 'react-simple-typewriter'
 
 function Home() {
+  const [text] = useTypewriter({
+    words: ['src/App.tsx'],
+    loop: 3,
+    typeSpeed: 500,
+    deleteSpeed: 0,
+    delaySpeed: 400
+  })
+
   return (
     <div className="App">
       <ParticlesBg />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{text}</code> and save to reload.
         </p>
         <a
           className="App-link"

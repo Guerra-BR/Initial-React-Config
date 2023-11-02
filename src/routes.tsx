35c9a639eb41f8dbf467e663939e824a61da8275
Guerra-BR <guerinohenrique@gmail.com>
2023-11-02 10:08:31 -0300

    Route-fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
-import Perfil from './Pages/Page2'
+import Page2 from './Pages/Page2'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/perfil" element={<Perfil />} />
+    <Route path="/Page2" element={<Page2 />} />
   </Routes>
 )
 
